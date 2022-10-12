f86728c03de2f166eeede90ff6d545517701f771
Aleksandr-Pod <stdfire@gmail.com>
2022-10-12 20:07:24 +0300

    delete product by Id

--- controllers/products/delete.js
+++ controllers/products/delete.js
@@ -1,5 +1,13 @@
-const deleteProduct = () => {
+const createError = require('../../helpers/createError');
+const {Product} = require('../../models/product');
 
+const deleteProduct = async(req, res) => {
+    const {id} = req.body;
+    const result = await Product.findByIdAndRemove(id);
+    if (!result) throw createError(404);
+    res.status(204).json({
+        message: `product id=${id} deleted successfull`
+    })
 }
 
 module.exports = deleteProduct;
--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/", auth, validation(joiAddProductSchema), ctrlWrapper(ctrl.addProduct));
 router.get("/", auth, ctrlWrapper(ctrl.getAllProducts));
-// router.delete("/", auth, ctrlWrapper(ctrl.remove));
+router.delete("/", auth, ctrlWrapper(ctrl.deleteProduct));
 // router.update("/", auth, validation(joiSchema), ctrlWrapper(ctrl.update))
 
 module.exports = router;
