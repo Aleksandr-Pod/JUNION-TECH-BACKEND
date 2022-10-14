a759f31f0060813ffb1d9c53745ec3ff329c4b20
Aleksandr-Pod <stdfire@gmail.com>
2022-10-14 15:41:03 +0300

    update returns new object

--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -3,7 +3,7 @@ const {Product} = require('../../models/product');
 const getAllProducts = async (req, res) => {
     const data = await Product.find({});
     res.status(200).json({
-        message: `find ${data.length} results`,
+        quantity: data.length,
         data
     })
 } 
