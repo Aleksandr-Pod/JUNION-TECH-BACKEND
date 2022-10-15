cc0378f57d80d5a79d7b0e5ef40d4d62398f9011
Aleksandr-Pod <stdfire@gmail.com>
2022-10-15 20:32:22 +0300

    addProduct check for existance

--- controllers/products/add.js
+++ controllers/products/add.js
@@ -1,7 +1,10 @@
+const createError = require('../../helpers/createError');
 const {Product} = require('../../models/product');
 
 const addProduct = async(req, res) => {
     const {name, price = 0, category = "unSorted", owner} = req.body;
+    const result = await Product.findOne({name});
+    if (result) throw createError(409, "The product already exist!");
     await Product.create({name, price, category: category.replace(' ').split(','), owner});
     res.status(201).json({
         message: "product added successfull",
