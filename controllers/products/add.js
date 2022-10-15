a854364d46463b98ddcb41fbcf678b6973403735
Aleksandr-Pod <stdfire@gmail.com>
2022-10-15 15:17:28 +0300

    split categories to array

--- controllers/products/add.js
+++ controllers/products/add.js
@@ -2,7 +2,7 @@ const {Product} = require('../../models/product');
 
 const addProduct = async(req, res) => {
     const {name, price = 0, category = "unSorted", owner} = req.body;
-    await Product.create({name, price, category, owner});
+    await Product.create({name, price, category: category.replace(' ').split(','), owner});
     res.status(201).json({
         message: "product added successfull",
         data: {name, price, category, owner}
--- controllers/products/update.js
+++ controllers/products/update.js
@@ -4,7 +4,7 @@ const {Product} = require('../../models/product');
 const updateProduct = async(req, res) => {
     const {id, name, price, category, owner} = req.body;
     if (!name && !price && !category) throw createError (400, 'no fields to update');
-    const result = await Product.findByIdAndUpdate(id, {name, price, category, owner}, {new:true});
+    const result = await Product.findByIdAndUpdate(id, {name, price, category: category.replace(" ").split(','), owner}, {new:true});
     if (!result) throw createError(404);
     res.status(200).json({
         message: 'updated successfull',
