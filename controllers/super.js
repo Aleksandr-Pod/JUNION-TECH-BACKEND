1a9e8c95737a1a1f52c5a5690082713ecb0eb40b
Aleksandr-Pod <stdfire@gmail.com>
2022-11-05 21:23:39 +0200

    update role and delete users by superAdmin

--- controllers/super.js
+++ controllers/super.js
@@ -4,11 +4,20 @@ const { User } = require("../models/user");
 
 const getSys = async(req, res) => {
     if (req.user.role !== "superadmin") throw createError(401, "super admin rights required");
-    const result = await Sys.find({}, {_id: 0});
+    const result = await Sys.findOne({}, {_id: 0});
     res.status(200).json({result})
 }
+const putSuperPass = async (req, res) => {
+    if (req.user.role !== "superadmin") throw createError(401, "super admin rights required");
+    const { superPass, role } = req.body;
+    if (!superPass && !role) createError(400, 'no fields to update')
+    await Sys.findByIdAndUpdate(process.env.SYS_ID, { superPass, role }, { new: true });
+    res.status(200).json({
+        superPass,
+        role
+    })
+}
 const getAllUsers = async (req, res) => {
-    console.log("getAllUsers: role - ", req.user.role);
     if (req.user.role !== "superadmin") throw createError(401, "super admin rights required");
     const result = await User.find({}, {password: 0, token: 0});
     res.status(200).json({
@@ -16,13 +25,23 @@ const getAllUsers = async (req, res) => {
         result
     })
 }
-const putSuperPass = async (req, res) => {
-    console.log("putSuoerPass: role - ", req.user.role);
+const updateUserRole = async (req, res) => {
     if (req.user.role !== "superadmin") throw createError(401, "super admin rights required");
-    const { superPass, role } = req.body;
-    await Sys.findByIdAndUpdate(process.env.SYS_ID, { superPass, role }, { new: true });
+    const { role, id } = req.body;
+    if (!role || !id) throw createError(400, "user's role and id required");
+    const result = await User.findByIdAndUpdate(id, { role });
+    if (!result) throw createError(404);
     res.status(200).json({
-        access: {superPass, role}
+        message: "the user's role updated successfully"
     })
 }
-module.exports = { getSys, getAllUsers, putSuperPass };
+const deleteUser = async (req, res) => {
+    if (req.user.role !== "superadmin") throw createError(401, "super admin rights required");
+    if (!req.body.id) createError(400, "user id required");
+    const result = await User.findByIdAndRemove(req.body.id);
+    if (!result) throw createError(404);
+    res.status(200).json({
+        message: `user deleted successfully`
+    })
+}
+module.exports = { getSys, putSuperPass, getAllUsers, updateUserRole, deleteUser};
--- routes/super.js
+++ routes/super.js
@@ -5,5 +5,7 @@ const router = require("express").Router();
 router.get('/', auth, ctrlWrapper(ctrl.getSys));
 router.put('/', auth, ctrlWrapper(ctrl.putSuperPass));
 router.get('/users', auth, ctrlWrapper(ctrl.getAllUsers));
+router.put('/users', auth, ctrlWrapper(ctrl.updateUserRole));
+router.delete('/users', auth, ctrlWrapper(ctrl.deleteUser));
 
 module.exports = router;
